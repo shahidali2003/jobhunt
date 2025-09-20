3df12902a1c7182c0245b89c594ba37b0d39291f
shahid ali <shahidali7644444@gmail.com>
2025-09-20 12:47:39 +0530

    Fix folder names and updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import Header from "./components/Header";
-import AddForm from "./components/Addform";
-import FilterBar from "./components/FilterBar";
-import Stats from "./components/Stats";
-import ApplicationCard from "./components/ApplicationCard";
+import Header from "./tempComponents/Header";
+import AddForm from "./tempComponents/AddForm";
+import FilterBar from "./Components/FilterBar";
+import Stats from "./Components/Stats";
+import ApplicationCard from "./Components/ApplicationCard";
 
 const App = () => {
   const [items, setItems] = useState(
